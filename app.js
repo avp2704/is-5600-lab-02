43f507b11ad6d9359849b5479ad26462113e1ad4
avp2704 <venkatapraveen.arisetty@slu.edu>
2024-10-30 06:55:17 +0000

    first_commit

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-/* add your code here */
-
+document.addEventListener('DOMContentLoaded', () => {
+        const stocksData = JSON.parse(stockContent);
+        const userData = JSON.parse(userContent);
+        generateUserList(userData, stocksData);
+        /**
+ * Loops through the users and renders a ul with li elements for each user
+ * @param {*} users 
+ */
+function generateUserList(users) {
+    // get the list element and for each user create a list item and append it to the list
+    const userList = document.querySelector('.user-list');
+    
+    users.map(({user, id}) => {
+      const listItem = document.createElement('li');
+      listItem.innerText = user.lastname + ', ' + user.firstname;
+      listItem.setAttribute('id', id);
+      userList.appendChild(listItem);
+    });
+  }
+  });
